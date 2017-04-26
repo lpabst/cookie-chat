6f94bbd1149926081f7bddfa80368ad69cee9b10
loren pabst <loren pabst>
2017-04-26 14:19:06 -0600

    date is displaying properly

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,5 +1,25 @@
 angular.module('chatroom').controller('mainCtrl', function($scope, messageService){
 
+  $scope.time = function(str){
+    var arr = str.split('');
+    var date = [];
+    for (var i = 5; i < 10; i++){
+      date.push(arr[i]);
+    }
+    date.push('-');
+    for(var i = 0; i < 4; i ++){
+      date.push(arr[i]);
+    }
+    date.push(', ');
+    for (var i = 11; i < 16; i ++){
+      date.push(arr[i]);
+    }
+
+
+    return date.join('');
+  }
+
+
   //The getMessages function will call the getMessages method on the messageService object.
   //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
   $scope.getMessages = function(){
